5e31d4d45e84756eeeba47cd4c72aa5fb86818ea
BAWORID <fazzafabianahmad@my.id>
2023-12-07 20:02:50 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-# Start SSH server
-service ssh start
-
-# Display IP address and username
-IP=$(hostname -I | awk '{print $1}')
-echo "SSH Server IP: ${IP}"
-echo "SSH Username: ${SSH_USER}"
-
-# Keep the container running
-tail -f /dev/null
+okteto exec -- bash -c "echo 'Username: $USER'; echo 'IP: $SSH_CLIENT'; echo 'Password: your_password'"
