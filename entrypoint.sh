9b56b8c234a9df0f785b43a7cb1cc5518c1bcbdd
BAWORID <fazzafabianahmad@my.id>
2023-12-07 20:51:13 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-echo "IP VPS Anda adalah:"
-curl ifconfig.me
+
+# Install Ngrok
+sudo apt-get update -y > /dev/null 2>&1 && apt-get install curl openssh-server -y > /dev/null 2>&1
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+chmod +x ngrok
+rm ngrok-stable-linux-amd64.zip
+
+# Authenticate Ngrok
+./ngrok authtoken [NGROK_AUTH_TOKEN]
+
+# Start SSH Tunnel
+./ngrok tcp [SSH_PORT] &
+
+# Wait for Ngrok to establish tunnel
+sleep 5
+
+# Retrieve Ngrok tunnel URL
+NGROK_URL=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "VPS SSH telah dibuat!"
+echo "Anda dapat mengaksesnya dengan menggunakan SSH client dan menghubungkan ke:"
+echo $NGROK_URL
