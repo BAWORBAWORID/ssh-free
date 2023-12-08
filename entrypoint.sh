89acdc59ac24ed3f67de77352d309de920d22293
BAWORID <fazzafabianahmad@my.id>
2023-12-08 09:19:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,17 @@
 # Install Ngrok
 sudo apt-get update -y > /dev/null 2>&1 && apt-get install curl openssh-server -y > /dev/null 2>&1
 
+
+
+# Mendapatkan alamat IP publik
+#ip=$(curl -s http://api.ipify.org)
+
+# Mendapatkan port SSH
+port=$(netstat -tuln | grep "ssh" | awk '{print $4}' | awk -F ":" '{print $NF}')
+
+echo "Alamat IP Anda adalah: $ip"
+echo "Port SSH yang digunakan adalah: $port"
+
 echo "VPS SSH telah dibuat!"
 echo "Anda dapat mengaksesnya dengan menggunakan SSH client dan menghubungkan ke:"
 curl -s https://checkip.amazonaws.com
